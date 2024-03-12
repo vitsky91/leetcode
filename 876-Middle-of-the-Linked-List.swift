725a2220afc59798029200a995d07095db8d79e9
Vitalii Sydorskyi <36030681+vitsky91@users.noreply.github.com>
2024-03-12 15:49:52 +0100

    Update 876-Middle-of-the-Linked-List.swift

--- 876-Middle-of-the-Linked-List.swift
+++ 876-Middle-of-the-Linked-List.swift
@@ -1,1 +1,55 @@
+/// Description:
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
 
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+Constraints:
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+*/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        if head?.next == nil {
+            return head
+        }
+        
+        var count: Double = 0
+        var currentNode: ListNode? = head
+
+        while currentNode != nil {
+            count += 1
+            currentNode = currentNode?.next
+        }
+
+        currentNode = head
+
+        for i in 1...Int((count / 2)) {
+            currentNode = currentNode?.next
+        }
+
+        return currentNode
+    }
+}
